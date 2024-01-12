ea7d752b240409bee1f7b89fccb88865b2113622
SalinasJulioC <salinasjulioc@hotmail.com>
2024-01-11 21:11:32 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TwarzAppMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
